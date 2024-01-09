b5506f1d5dbd7822b0288fd93e094cafe19d8eca
Adityag025 <74089225+Adityag025@users.noreply.github.com>
2024-01-09 22:59:22 +0530

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-include('function.inc.php');
-unset($_SESSION['FOOD_USER_ID']);
-unset($_SESSION['FOOD_USER_NAME']);
-redirect('shop');
+include('../function.inc.php');
+unset($_SESSION['IS_LOGIN']);
+unset($_SESSION['ADMIN_USER']);
+redirect('login.php');
 ?>
